377a76e5c3335c72e0188e3ec6a962f640c031f2
Soham Nimbalkar <sohamnimbalkar07@gmail.com>
2024-11-03 21:20:48 +0530

    updating recoil code

---
+++ Recoil/RecoilCounter/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import "./App.css";
+import { countAtom } from "./store/atom/count";
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+
+function App() {
+  return (
+    <div>
+      <RecoilRoot>
+      <Count />
+      </RecoilRoot>
+    </div>
+  );
+}
+
+function Count() {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  return <div>{count}</div>;
+}
+
+function Buttons() {
+  const [ count, setCount] = useRecoilState(countAtom);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrement
+      </button>
+    </div>
+  );
+}
+
+export default App;
